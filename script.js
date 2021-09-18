a8ec6cf93d77031bd3214cdbfcc40d1518ff7c59
o_sam <ollie.samuel11@gmail.com>
2021-09-18 12:26:48 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-for (let i = 0;  i<=100; i++) {
-    if (i % 3 ==0 && i % 5 == 0 ) {
-        console.log("FizzBuzz");
-    }
-    else if (i % 3 == 0) {
-        console.log("Fizz");
-    }
-    else if (i % 5 == 0) {
-        console.log("Buzz");
-    }
-    else {
-        console.log (i);
+for ( i=1; i<=20; i++) {
+    if (i%2!=0) {
+        console.log(i)
     }
 }
+
+for (i=100; i>=0; i--) {
+    if(i%3==0){
+        console.log(i)
+    }
+}
+
+var x = 5.5;
+    for (i = 0; i < 6; i++) {
+        var x = x-1.5;
+        console.log(x);
+    }
+
+var x = 0
+    for (i = 0; i <= 100; i++) {
+        var x = x += i;
+        console.log(x)
+    }
+
+var x = 1
+    for (i = 1; i  <= 12; i++) {
+        var x = x *= i;
+        console.log(x)
+    }
+
